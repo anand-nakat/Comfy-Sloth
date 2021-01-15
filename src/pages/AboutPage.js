88e3e26e5713a21aec89d0ec53cb82695268f133
Anand Nakat <nakatanand2000@gmail.com>
2021-01-15 10:56:51 +0530

    Error/About/Checkout Page Initialized

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="About Image" />
+        <article>
+          <div className="title">
+            <h2>Our Story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            nostrum doloribus nobis accusamus rerum a, illo unde magnam aliquam
+            blanditiis asperiores, voluptatibus dolorum suscipit odit nesciunt
+            minus sed at! Possimus facilis dicta id ullam omnis debitis iusto
+            tenetur veritatis dolorum?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +53,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
-  return <h4>error page</h4>
-}
+  return (
+    <Wrapper className="page-100">
+      <h1>404</h1>
+      <h3>
+        Sorry, the page you were looking for could not be found on the server
+      </h3>
+      <Link to="/" className="btn">
+        Back to Home
+      </Link>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.main`
   background: var(--clr-primary-10);
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
@@ -18,6 +29,6 @@ const Wrapper = styled.main`
     text-transform: none;
     margin-bottom: 2rem;
   }
-`
+`;
 
-export default ErrorPage
+export default ErrorPage;
